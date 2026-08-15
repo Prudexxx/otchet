4b5be269853dab32de97894c51501dcea2bd223d
Prudexxx <105013402+Prudexxx@users.noreply.github.com>
2026-08-16 02:11:23 +0500

    Add files via upload

---
+++ on.js
@@ -0,0 +1,3 @@
+        const ADMIN_PASSWORD = "1234"; 
+        // ВАША ССЫЛКА НА GOOGLE APPS SCRIPT
+       const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyTIvFS5UTEypdqxN8OXw4pke0DbAiSVmfizE1CVegVF0CxfmsC2bTXxul8W-6pNMd9/exec';
